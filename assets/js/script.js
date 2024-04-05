603aac58aafb39071fa131a0e1752e515ab48161
Alex LaPierre <aslapierre7@gmail.com>
2024-04-05 00:36:32 -0500

    added containers for search, search results, search history, added comments for what to do in js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Find a weather api
+// Use different selectors to traverse to get:
+// Current weather and future weather
+// Temp, humidity, wind speed
+// Icon representation for weather (font awesome??)
+
+
+// Search history???
