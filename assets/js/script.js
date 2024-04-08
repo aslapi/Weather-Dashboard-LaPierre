b274db4efb81fa06debfeb341cf8ff5303b0d65c
Alex LaPierre <aslapierre7@gmail.com>
2024-04-08 18:26:36 -0500

    testing weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,21 @@
 // Temp, humidity, wind speed
 // Icon representation for weather (font awesome??)
 
+const weatherUrl = 'https://www.api.weatherapi.com/v1/current.json?key=782415be1aa6495db2b125628240704&q=Minneapolis'
 
 // Search history???
+function getApi() {
+
+    fetch(weatherUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+}
+
+
+
+
+//     "Authorization" : "782415be1aa6495db2b125628240704";
